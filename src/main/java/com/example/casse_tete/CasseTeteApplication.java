5c7613d5c6a0135b2f9e065c57467ad9bbee9a8c
Torea <p.torea@icloud.com>
2025-03-09 14:20:45 +0400

    first commit

---
+++ src/main/java/com/example/casse_tete/CasseTeteApplication.java
@@ -0,0 +1,13 @@
+package com.example.casse_tete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasseTeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasseTeteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/casse_tete/CasseTeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casse_tete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasseTeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
