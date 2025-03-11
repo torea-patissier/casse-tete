368ca811d7c86afb629c6a4702ef9983da322580
Torea <p.torea@icloud.com>
2025-03-11 14:55:01 +0400

    feat: controller

---
+++ src/main/java/com/example/casse_tete/controller/BookController.java
@@ -0,0 +1,88 @@
+package com.example.casse_tete.controller;
+
+import com.example.casse_tete.model.Book;
+import com.example.casse_tete.repo.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class BookController {
+    @Autowired
+    private BookRepo bookRepo;
+
+    @GetMapping("/getAllBooks")
+    public ResponseEntity<List<Book>> getAllBooks(){
+        try {
+            List<Book> bookList = new ArrayList<>();
+            bookRepo.findAll().forEach(bookList::add);
+
+            if(bookList.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(bookList, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getBookById/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id){
+        try {
+            Optional<Book> bookdata = bookRepo.findById(id);
+
+            if(bookdata.isPresent()){
+                return new ResponseEntity<>(bookdata.get(), HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/addBook")
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        try {
+            Book bookObj = bookRepo.save(book);
+            return new ResponseEntity<>(bookObj, HttpStatus.CREATED);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/updateBookById/{id}")
+    public ResponseEntity<Book> updateBookById(@PathVariable Long id, @RequestBody Book newBookData){
+        try {
+            Optional<Book> oldBookData = bookRepo.findById(id);
+
+            if(oldBookData.isPresent()){
+                Book updatedBookData = oldBookData.get();
+                updatedBookData.setTitle(newBookData.getTitle());
+                updatedBookData.setAuthor(newBookData.getAuthor());
+                Book bookObj = bookRepo.save(updatedBookData);
+            return new ResponseEntity<>(bookObj, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/deleteBookById/{id}")
+    public ResponseEntity<HttpStatus> deleteBookById(@PathVariable Long id){
+        try {
+            bookRepo.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
