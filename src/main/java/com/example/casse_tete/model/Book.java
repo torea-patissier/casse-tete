056e1a21c81a3ce87fa0e61e2e5e47fc1b035646
Torea <p.torea@icloud.com>
2025-03-11 17:45:06 +0400

    feat: add anotation

--- src/main/java/com/example/casse_tete/model/Book.java
+++ src/main/java/com/example/casse_tete/model/Book.java
@@ -11,13 +11,11 @@ import lombok.*;
 @Getter
 @ToString
 public class Book {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String author;
-
-
 }
