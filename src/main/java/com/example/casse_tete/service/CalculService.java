7ecba1e36a32731521f03353c80777c2d0f36542
Torea <p.torea@icloud.com>
2025-03-12 12:04:30 +0400

    feat: separate logic between calul and solution

---
+++ src/main/java/com/example/casse_tete/service/CalculService.java
@@ -0,0 +1,70 @@
+package com.example.casse_tete.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculService {
+
+    private static final int FIRST_INPUT = 13;
+    private static final int SECOND_INPUT = 12;
+    private static final int THIRD_INPUT = 11;
+    private static final int FOURTH_INPUT = 10;
+
+    static JsonNode parseJson(String gridData) {
+        try {
+            return new ObjectMapper().readTree(gridData);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error while parsing gridData : " + e);
+        }
+    }
+
+    public int calculA(JsonNode jsonNode){
+        try {
+            int B = jsonNode.get("B").asInt();
+            int C = jsonNode.get("C").asInt();
+            return (FIRST_INPUT * B) / C;
+        } catch (Exception ex) {
+            throw new RuntimeException("Error while calculating A :" + ex.getMessage());
+        }
+    }
+
+    public int calculB(JsonNode jsonNode){
+        try{
+            int E = jsonNode.get("E").asInt();
+            return SECOND_INPUT * E;
+        } catch (RuntimeException ex) {
+            throw new RuntimeException("Error while calculating B :" + ex.getMessage());
+        }
+    }
+
+    public int calculC(JsonNode jsonNode){
+        try{
+            int G = jsonNode.get("G").asInt();
+            int H = jsonNode.get("H").asInt();
+            int I = jsonNode.get("I").asInt();
+            return (G * H) / I;
+        } catch (RuntimeException ex) {
+            throw new RuntimeException("Error while calculating C :" + ex.getMessage());
+        }
+    }
+
+    public int calculResult(JsonNode jsonNode){
+        try {
+            int A = jsonNode.get("A").asInt();
+            int D = jsonNode.get("D").asInt();
+            int F = jsonNode.get("F").asInt();
+
+            int aResult = calculA(jsonNode);
+            int bResult = calculB(jsonNode);
+            int cResult = calculC(jsonNode);
+
+            int result = A + aResult + D + bResult - F - THIRD_INPUT + cResult - FOURTH_INPUT;
+            return result;
+        } catch (Exception ex) {
+            throw new RuntimeException("Error while calculating result");
+        }
+    }
+}
--- src/main/java/com/example/casse_tete/service/SolutionService.java
+++ src/main/java/com/example/casse_tete/service/SolutionService.java
@@ -2,9 +2,6 @@ package com.example.casse_tete.service;
 
 import com.example.casse_tete.model.Solution;
 import com.example.casse_tete.repo.SolutionRepo;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,70 +12,13 @@ import java.util.Optional;
 public class SolutionService {
 
     private final SolutionRepo solutionRepo;
-    private static final int FIRST_INPUT = 13;
-    private static final int SECOND_INPUT = 12;
-    private static final int THIRD_INPUT = 11;
-    private static final int FOURTH_INPUT = 10;
     private static final int EXPECTED_RESULT = 66;
+    private final CalculService calculService;
 
     @Autowired
-    public SolutionService(SolutionRepo solutionRepo) {
+    public SolutionService(SolutionRepo solutionRepo, CalculService calculService) {
         this.solutionRepo = solutionRepo;
-    }
-
-    private static JsonNode parseJson(String gridData) {
-        try {
-            return new ObjectMapper().readTree(gridData);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error while parsing gridData : " + e);
-        }
-    }
-
-    private int calculA(JsonNode jsonNode){
-        try {
-            int B = jsonNode.get("B").asInt();
-            int C = jsonNode.get("C").asInt();
-            return (FIRST_INPUT * B) / C;
-        } catch (Exception ex) {
-            throw new RuntimeException("Error while calculating alpha :" + ex.getMessage());
-        }
-    }
-
-    private int calculB(JsonNode jsonNode){
-        try{
-            int E = jsonNode.get("E").asInt();
-            return SECOND_INPUT * E;
-        } catch (RuntimeException ex) {
-            throw new RuntimeException("Error while calculating bravo :" + ex.getMessage());
-        }
-    }
-
-    private int calculC(JsonNode jsonNode){
-        try{
-            int G = jsonNode.get("G").asInt();
-            int H = jsonNode.get("H").asInt();
-            int I = jsonNode.get("I").asInt();
-            return (G * H) / I;
-        } catch (RuntimeException ex) {
-            throw new RuntimeException("Error while calculating charlie :" + ex.getMessage());
-        }
-    }
-
-    private int calculateResult(JsonNode jsonNode){
-        try {
-            int A = jsonNode.get("A").asInt();
-            int D = jsonNode.get("D").asInt();
-            int F = jsonNode.get("F").asInt();
-
-            int aResult = calculA(jsonNode);
-            int bResult = calculB(jsonNode);
-            int cResult = calculC(jsonNode);
-
-            int result = A + aResult + D + bResult - F - THIRD_INPUT + cResult - FOURTH_INPUT;
-            return result;
-        } catch (Exception ex) {
-            throw new RuntimeException("Error while calculating result");
-        }
+        this.calculService = calculService;
     }
 
     public Solution postSolution(Solution solution) {
@@ -88,7 +28,7 @@ public class SolutionService {
         }
 
         try {
-            int result = calculateResult(parseJson(solution.getGridData()));
+            int result = calculService.calculResult(calculService.parseJson(solution.getGridData()));
             if(result == EXPECTED_RESULT){
                 solution.setCorrect(true);
             }
@@ -133,9 +73,6 @@ public class SolutionService {
         if (existingSolution.isEmpty()) {
             throw new RuntimeException("Solution with ID " + solution.getId() + " not found");
         }
-
         return postSolution(solution);
     }
-
-
 }
